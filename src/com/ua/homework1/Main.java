6a0af925807526e7a001ab808daca89439c472e6
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2023-01-17 23:07:56 +0100

    Homework 2 completed

--- src/com/ua/homework1/Main.java
+++
@@ -1,7 +0,0 @@
-package com.ua.homework1;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
-    }
-}
---
+++ src/com/ua/homework2/Calculations.java
@@ -0,0 +1,12 @@
+package com.ua.homework2;
+
+public class Calculations {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 3;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+    }
+}
