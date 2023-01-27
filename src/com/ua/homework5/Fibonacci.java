1969ed5cbc4a242ae850e55c21dc39a25f372e45
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2023-01-27 22:35:15 +0100

    Homework 5 completed

--- src/com/ua/homework5/Fibonacci.java
+++ src/com/ua/homework5/Fibonacci.java
@@ -5,11 +5,11 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        int n = myObj.nextInt();
+        int number = myObj.nextInt();
         int firstNum = 0;
         int secondNum = 1;
         int thirdNum;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < number; i++) {
             System.out.println(firstNum);
             thirdNum = firstNum + secondNum;
             firstNum = secondNum;
