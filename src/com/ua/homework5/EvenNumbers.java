985bae017a297a3ba2072c8db24f119508716abf
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2023-01-27 22:32:47 +0100

    Homework 5 completed

---
+++ src/com/ua/homework5/EvenNumbers.java
@@ -0,0 +1,11 @@
+package com.ua.homework5;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/com/ua/homework5/Fibonacci.java
@@ -0,0 +1,22 @@
+package com.ua.homework5;
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = myObj.nextInt();
+        int firstNum = 0;
+        int secondNum = 1;
+        int thirdNum;
+        for (int i = 0; i < n; i++) {
+            System.out.println(firstNum);
+            thirdNum = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = thirdNum;
+            if (firstNum > 100) {
+                break;
+            }
+        }
+    }
+}
