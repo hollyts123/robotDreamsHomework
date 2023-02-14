3c5fb0be19831aa7c1efb0890b0d34f2f47653cf
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2023-02-14 22:39:30 +0100

    Homework 9 completed

---
+++ src/com/ua/homework9/Car.java
@@ -0,0 +1,8 @@
+package com.ua.homework9;
+
+public class Car extends Vehicle {
+    @Override
+    protected String returnVehicleType() {
+        return "I'm a car!";
+    }
+}
---
+++ src/com/ua/homework9/Truck.java
@@ -0,0 +1,8 @@
+package com.ua.homework9;
+
+public class Truck extends Vehicle{
+    @Override
+    protected String returnVehicleType() {
+        return "I'm a truck!";
+    }
+}
